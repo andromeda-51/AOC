2ac0aa97de91c8c093bf6a29a7937c34b9a7e725
andromeda-51 <michaelpf97@gmail.com>
2023-01-23 23:04:14 +0000

    day 1 2015 part 2

--- AOC_2015/src/main.rs
+++ AOC_2015/src/main.rs
@@ -1,3 +1,36 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+fn read_input(filename: &str) -> String {
+        let path = Path::new(filename);
+        let display = path.display();
+
+        let mut file = match File::open(&path) {
+                Err(why) => panic!("Could not open {}: {}", display, why),
+                Ok(file) => file,
+        };
+
+        let mut s = String::new();
+        match file.read_tostring(&mut s) {
+                Err(why) => panic!("Could not read {}: {}", display, why),
+                Ok() => (),
+        };
+        s
+}
+
 fn main() {
-    println!("Hello, world!");
+        let input = readinput("input.txt");
+        let mut result = 0;
+        for (x, i) in input.chars().enumerate() {
+                result = match i {
+                        '(' => result + 1,
+                        ')' => result - 1,
+                         => result,
+                };
+                if result < 0 { 
+                        println!("{}", x + 1);
+                        break;
+                };
+        };
 }
